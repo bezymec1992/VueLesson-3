495a1a12e660a633cfbb255a5c20da90f25fd95a
VasylLavrov <bbezzymecc@gmail.com>
2024-05-24 23:52:57 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Card from './components/Card.vue'
+</script>
+
+<template>
+  <Card title="some title" text="Some text"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+defineProps({
+  title: {
+    type: String,
+    required: Boolean
+  },
+  text: {
+    type: String,
+    required: Boolean
+  }
+})
+</script>
+
+<template>
+  <div className="card">
+   <div className="card-body">
+     <h4 v-if="title" className="card-title card">11111{{ title }}</h4>
+     <p v-if="text" className="card-text">2222{{ text }}</p>
+   </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
