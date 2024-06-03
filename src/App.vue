676ddf4552cf8ff8f75d643aef767e4966cea12e
VasylLavrov <bbezzymecc@gmail.com>
2024-06-04 00:19:10 +0700

    alert task

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AlertVue :type="type"  :text="text"/>;
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AlertVue from './components/AlertVue.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      text: 'What is love?',
+      type: 'warning'
+    }
+  },
   components: {
-    HelloWorld
+    AlertVue
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/AlertVue.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="'alert alert-' + type" role="alert">{{ text }}</div>
+</template>
+
+<script>
+export default {
+  name: 'AlertVue',
+  props: {
+    text: {
+      type: String,
+      required:true
+    },
+    type: {
+      type: String,
+      default: "danger"
+    }
+    
+  }
+}
+
+</script>
+
+<style scoped>
+ 
+</style>
