52c5625d836d868cb2066b3e9e5a3c224e0fcd61
VasylLavrov <bbezzymecc@gmail.com>
2024-07-03 23:33:56 +0100

    commit-13

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
-  <AlertVue :type="type"  :text="text"/>;
+  <AlertVue :type="type"  :text="text"/>
+  <BtnGroup />
 </template>
 
 <script>
 import AlertVue from './components/AlertVue.vue'
+import BtnGroup from './components/BtnGroup.vue'
 
 export default {
   name: 'App',
   data() {
     return {
-      text: 'What is love?',
+      text: 'What is love?asasdas',
       type: 'warning'
     }
   },
   components: {
-    AlertVue
+    AlertVue,
+    BtnGroup
   }
 }
 </script>
---
+++ src/components/BtnGroup.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="btn-group">
+    <button type="button"
+      :class="['btn', 'btn-secondary', 'left', activeBtn === 'left' ? 'active' : '']"
+      @click="setActive('left')"
+    >
+      Button 1
+    </button>
+    <button type="button"
+      :class="['btn', 'btn-secondary', 'right', activeBtn === 'right' ? 'active' : '']"
+      @click="setActive('right')"
+    >
+      Button 2
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BtnGroup',
+  data() {
+    return {
+      activeBtn: null,
+    };
+  },
+  methods: {
+    setActive(btn) {
+      this.activeBtn = btn
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+ 
+</style>
